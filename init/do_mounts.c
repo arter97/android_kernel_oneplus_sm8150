21843b16dd1a63efcb46b436202f0c3d50363b22
Park Ju Hyung <qkrwngud825@gmail.com>
2018-10-30 01:45:20 +0900

    init: bypass dm-verity and mount system directly
    Mount system_a/system_b directly by reading androidboot.slot_suffix=
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -40,7 +40,7 @@ int __initdata rd_doload;	/* 1 = load RAM disk, 0 = don't load */
 
 int root_mountflags = MS_RDONLY | MS_SILENT;
 static char * __initdata root_device_name;
-static char __initdata saved_root_name[64];
+static char __initdata saved_root_name[64] = "PARTLABEL=system";
 static int root_wait;
 #ifdef CONFIG_EARLY_SERVICES
 static char saved_modem_name[64] __initdata;
@@ -385,11 +385,11 @@ EXPORT_SYMBOL_GPL(name_to_dev_t);
 
 static int __init root_dev_setup(char *line)
 {
-	strlcpy(saved_root_name, line, sizeof(saved_root_name));
+	strncat(saved_root_name, line, 3);
 	return 1;
 }
 
-__setup("root=", root_dev_setup);
+__setup("androidboot.slot_suffix=", root_dev_setup);
 
 #ifdef CONFIG_EARLY_SERVICES
 static int __init modem_dev_setup(char *line)
