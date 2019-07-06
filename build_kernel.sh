ca98321ce860a0ff9ea7393cbf042bdc7c32404c
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-07 03:16:21 +0900

    Add build tools
    And use Magisk for now
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ build_kernel.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
+export PARTITION_SIZE=67108864
+
+export OS="9.0.0"
+export SPL="2019-05"
+
+echo "kerneldir = $KERNELDIR"
+echo "ramfs_source = $RAMFS_SOURCE"
+
+RAMFS_TMP="/tmp/arter97-op7-ramdisk"
+
+echo "ramfs_tmp = $RAMFS_TMP"
+cd $KERNELDIR
+
+stock=0
+if [[ "${1}" == "stock" ]] ; then
+	stock=1
+	shift
+fi
+
+if [[ "${1}" == "skip" ]] ; then
+	echo "Skipping Compilation"
+else
+	if [[ "$stock" == "1" ]] ; then
+		sed -i -e 's@"want_initramfs"@"skip_initramfs"@g' init/initramfs.c
+	fi
+	echo "Compiling kernel"
+	cp defconfig .config
+	make "$@" || exit 1
+	if [[ "$stock" == "1" ]] ; then
+		sed -i -e 's@"skip_initramfs"@"want_initramfs"@g' init/initramfs.c
+	fi
+fi
+
+echo "Building new ramdisk"
+#remove previous ramfs files
+rm -rf '$RAMFS_TMP'*
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+#copy ramfs files to tmp directory
+cp -axpP $RAMFS_SOURCE $RAMFS_TMP
+cd $RAMFS_TMP
+
+#clear git repositories in ramfs
+find . -name .git -exec rm -rf {} \;
+find . -name EMPTY_DIRECTORY -exec rm -rf {} \;
+
+if [[ "$stock" == "1" ]] ; then
+	# Don't use Magisk
+	mv .backup/init init
+	rm -rf .backup
+fi
+
+$KERNELDIR/ramdisk_fix_permissions.sh 2>/dev/null
+
+cd $KERNELDIR
+rm -rf $RAMFS_TMP/tmp/*
+
+cd $RAMFS_TMP
+find . | fakeroot cpio -H newc -o | gzip -9 > $RAMFS_TMP.cpio.gz
+ls -lh $RAMFS_TMP.cpio.gz
+cd $KERNELDIR
+
+echo "Making new boot image"
+mkbootimg \
+    --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb \
+    --ramdisk $RAMFS_TMP.cpio.gz \
+    --cmdline 'androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user printk.devkmsg=on' \
+    --base           0x00000000 \
+    --pagesize       4096 \
+    --kernel_offset  0x00008000 \
+    --ramdisk_offset 0x01000000 \
+    --second_offset  0x00f00000 \
+    --tags_offset    0x00000100 \
+    --os_version     $OS \
+    --os_patch_level $SPL \
+    --header_version 1 \
+    -o $KERNELDIR/boot.img
+
+GENERATED_SIZE=$(stat -c %s boot.img)
+if [[ $GENERATED_SIZE -gt $PARTITION_SIZE ]]; then
+	echo "boot.img size larger than partition size!" 1>&2
+	exit 1
+fi
+
+echo "done"
+ls -al boot.img
+echo ""
---
+++ build_master.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [[ "${1}" != "skip" ]] ; then
+	./build_clean.sh
+	./build_kernel.sh stock "$@" || exit 1
+fi
+
+VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+
+if [ -e boot.img ] ; then
+	rm arter97-kernel-$VERSION.zip 2>/dev/null
+	cp boot.img arter97-kernel-$VERSION.img
+
+	# Pack AnyKernel2
+	rm -rf kernelzip
+	mkdir -p kernelzip/dtbs
+	cp arch/arm64/boot/Image.gz kernelzip/
+	find arch/arm64/boot -name '*.dtb' -exec cp {} kernelzip/dtbs/ \;
+	echo "
+kernel.string=arter97 kernel $(cat version) @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus7
+device.name2=OnePlus7Pro
+device.name3=guacamoleb
+device.name4=guacamole
+block=/dev/block/bootdevice/by-name/boot
+is_slot_device=1
+ramdisk_compression=gzip
+" > kernelzip/props
+	cp -rp ~/android/anykernel/* kernelzip/
+	cd kernelzip/
+	7z a -mx0 arter97-kernel-$VERSION-tmp.zip *
+	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
+	rm arter97-kernel-$VERSION-tmp.zip
+	cd ..
+	ls -al arter97-kernel-$VERSION.zip
+fi
