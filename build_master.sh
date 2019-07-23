f810405c089d3aec031c70850bd4726729b9952c
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-24 02:55:54 +0900

    build_master: use legacy lz4 for AnyKernel
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -28,7 +28,7 @@ device.name3=guacamoleb
 device.name4=guacamole
 block=/dev/block/bootdevice/by-name/boot
 is_slot_device=1
-ramdisk_compression=gzip
+ramdisk_compression=lz4-l
 " > kernelzip/props
 	cp -rp ~/android/anykernel/* kernelzip/
 	cd kernelzip/
