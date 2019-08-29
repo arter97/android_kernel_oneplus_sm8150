2307bf1971e317df3a48dcce80957ae74ebc8732
Ajay Agarwal <ajaya@codeaurora.org>
2019-08-29 19:27:55 +0530

    scsi: ufs: Fix power management OPs handling
    Currently, the ufshcd driver does forceful resume and suspend
    from system_suspend if device is runtime suspended. Next, when
    the system resume is invoked, it finds that the runtime status is
    still suspended and bails out. This leads to the runtime PM
    framework throwing errors when we try to set the runtime status
    of the children as active. This leads to the hardware going into
    a bad state.
    Fix this by doing a forceful resume as a part of system_resume
    and also marking the runtime status of ufshcd dev as ACTIVE.
    
    Change-Id: Id4454a2f32f3980ad7f8df62815fba4e5ce43ebe
    Signed-off-by: Ajay Agarwal <ajaya@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -10483,14 +10483,18 @@ int ufshcd_system_resume(struct ufs_hba *hba)
 	if (!hba)
 		return -EINVAL;
 
-	if (!hba->is_powered || pm_runtime_suspended(hba->dev))
+	if (!hba->is_powered)
 		/*
 		 * Let the runtime resume take care of resuming
 		 * if runtime suspended.
 		 */
 		goto out;
-	else
-		ret = ufshcd_resume(hba, UFS_SYSTEM_PM);
+
+	ret = ufshcd_resume(hba, UFS_SYSTEM_PM);
+	pm_runtime_disable(hba->dev);
+	pm_runtime_set_active(hba->dev);
+	pm_runtime_enable(hba->dev);
+
 out:
 	trace_ufshcd_system_resume(dev_name(hba->dev), ret,
 		ktime_to_us(ktime_sub(ktime_get(), start)),
