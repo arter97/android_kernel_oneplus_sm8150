831c4daaf8cb4329f47278561fcfd90f8d6e7050
Catalin Marinas <catalin.marinas@arm.com>
2019-09-25 16:49:04 -0700

    mm: untag user pointers in mmap/munmap/mremap/brk
    There isn't a good reason to differentiate between the user address space
    layout modification syscalls and the other memory permission/attributes
    ones (e.g.  mprotect, madvise) w.r.t.  the tagged address ABI.  Untag the
    user addresses on entry to these functions.
    
    Link: http://lkml.kernel.org/r/20190821164730.47450-2-catalin.marinas@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Will Deacon <will@kernel.org>
    Acked-by: Andrey Konovalov <andreyknvl@google.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Szabolcs Nagy <szabolcs.nagy@arm.com>
    Cc: Kevin Brodsky <kevin.brodsky@arm.com>
    Cc: Dave P Martin <Dave.Martin@arm.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -203,6 +203,8 @@ SYSCALL_DEFINE1(brk, unsigned long, brk)
 	bool downgraded = false;
 	LIST_HEAD(uf);
 
+	brk = untagged_addr(brk);
+
 	if (down_write_killable(&mm->mmap_sem))
 		return -EINTR;
 
@@ -1627,6 +1629,8 @@ SYSCALL_DEFINE6(mmap_pgoff, unsigned long, addr, unsigned long, len,
 	struct file *file = NULL;
 	unsigned long retval;
 
+	addr = untagged_addr(addr);
+
 	if (!(flags & MAP_ANONYMOUS)) {
 		audit_mmap_fd(fd, flags);
 		file = fget(fd);
@@ -2951,6 +2955,7 @@ EXPORT_SYMBOL(vm_munmap);
 
 SYSCALL_DEFINE2(munmap, unsigned long, addr, size_t, len)
 {
+	addr = untagged_addr(addr);
 	profile_munmap(addr);
 	return __vm_munmap(addr, len, true);
 }
