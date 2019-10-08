fbb8e0c08b828cf9a6ac0b7c83cf60f676a84dd3
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-08 23:47:15 +0900

    build_master: use level 9 compression for everything except Image.gz
    This saves 2MB of space
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -14,7 +14,6 @@ if [ -e boot.img ] ; then
 	# Pack AnyKernel3
 	rm -rf kernelzip
 	mkdir kernelzip
-	cp arch/arm64/boot/Image.gz kernelzip/
 	echo "
 kernel.string=arter97 kernel $(cat version) @ xda-developers
 do.devicecheck=1
@@ -32,7 +31,8 @@ ramdisk_compression=gzip
 	cp -rp ~/android/anykernel/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip *
+	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
+	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
 	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
 	rm arter97-kernel-$VERSION-tmp.zip
 	cd ..
