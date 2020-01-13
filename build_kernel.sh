e04f0e132c82af8a3a4f0a5fb1f58d53e61e2d80
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-14 00:44:39 +0900

    build_kernel: arter97 personal builds
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -68,7 +68,7 @@ find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > $RAMFS_TMP.dtb
 mkbootimg \
     --kernel $KERNELDIR/arch/arm64/boot/Image.gz \
     --ramdisk $RAMFS_TMP.cpio.lz4 \
-    --cmdline 'androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user printk.devkmsg=on' \
+    --cmdline 'androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user printk.devkmsg=on arter97' \
     --base           0x00000000 \
     --pagesize       4096 \
     --kernel_offset  0x00008000 \
