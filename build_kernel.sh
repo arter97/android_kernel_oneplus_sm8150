b23c3f9f320f9979fe13b47c5ac8d5e99a21cdf2
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-14 00:54:45 +0900

    build_kernel: load Magisk for personal builds
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -65,8 +65,9 @@ cd $KERNELDIR
 
 echo "Making new boot image"
 find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > $RAMFS_TMP.dtb
+cat $KERNELDIR/arch/arm64/boot/Image | sed s/skip_initramfs/want_initramfs/g | pigz -1 -R > $RAMFS_TMP.bin.gz
 mkbootimg \
-    --kernel $KERNELDIR/arch/arm64/boot/Image.gz \
+    --kernel $RAMFS_TMP.bin.gz \
     --ramdisk $RAMFS_TMP.cpio.lz4 \
     --cmdline 'androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user printk.devkmsg=on arter97' \
     --base           0x00000000 \
