d03f45255d3f349fbc6a89cdfc7b9ec7631166c3
Park Ju Hyung <qkrwngud825@gmail.com>
2020-06-05 22:10:56 +0900

    Revert "drm: allow dc dimming code to run"
    This reverts commit c99378bfc59623e9f83cf689a750ae89e1a86d6f.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -5778,14 +5778,20 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 			fppressed_index = i;
 			fppressed_index_rt = i;
 		}
-                if (mode ==3)
-                    aod_index = i;
+        if (mode ==3)
+            aod_index = i;
 	}
 
-	if(fp_index >=0 && dim_mode!=0)
+	if (fp_mode == 1) {
 		display->panel->dim_status = true;
-	else
+		cstate->fingerprint_pressed = true;
+		return 0;
+	} else {
 		display->panel->dim_status = false;
+		cstate->fingerprint_pressed = false;
+		cstate->fingerprint_dim_layer = NULL;
+		return 0;
+	}
 
 	if(aod_index <0){
 		oneplus_aod_hid = 0;
@@ -5929,15 +5935,6 @@ static int sde_crtc_onscreenfinger_atomic_check(struct sde_crtc_state *cstate,
 		cstate->fingerprint_dim_layer = NULL;
 	}
 
-        if (fp_mode == 1) {
-                display->panel->dim_status = true;
-                cstate->fingerprint_pressed = true;
-                return 0;
-        } else if (fp_mode == 0) {
-                display->panel->dim_status = false;
-                cstate->fingerprint_pressed = false;
-                return 0;
-        }
 	return 0;
 }
 
