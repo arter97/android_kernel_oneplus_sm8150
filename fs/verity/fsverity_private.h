c58dbddb9ba109d525e7ddc22947497fb186ab73
Park Ju Hyung <qkrwngud825@gmail.com>
2020-08-09 07:22:03 +0900

    Revert "fs-verity: remove unnecessary extern keywords"
    This reverts commit bc990c874500cb2fc2cbc377f9854562ff309b5d.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- fs/verity/fsverity_private.h
+++ fs/verity/fsverity_private.h
@@ -134,7 +134,7 @@ void __init fsverity_check_hash_algs(void);
 
 /* init.c */
 
-void __printf(3, 4) __cold
+extern void __printf(3, 4) __cold
 fsverity_msg(const struct inode *inode, const char *level,
 	     const char *fmt, ...);
 
