95a22451e003aad8156837558a68d33b408c9b21
Park Ju Hyung <qkrwngud825@gmail.com>
2020-08-09 07:16:19 +0900

    Revert "selinux: don't require auditing"
    This reverts commit dd8dfb067f155074291401a4c167f4e547935049.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3396,7 +3396,6 @@ out:
 	return match;
 }
 
-#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3419,7 +3418,6 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
-#endif
 
 #ifdef CONFIG_NETLABEL
 /**
