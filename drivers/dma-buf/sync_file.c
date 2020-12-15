004a80009563596ad5c4dd81f8af65b9ac9362b3
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-12-15 14:32:37 +0530

    Revert "dma-buf: Avoid dynamic memory allocation for small info buffers"
    This reverts commit 5a13e4aa2075012dff918c29dd3c81885952ad2f.

--- drivers/dma-buf/sync_file.c
+++ drivers/dma-buf/sync_file.c
@@ -414,7 +414,6 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 {
 	struct sync_file_info info;
 	struct sync_fence_info *fence_info = NULL;
-	struct sync_fence_info fence_info_onstack[4] __aligned(8);
 	struct dma_fence **fences;
 	__u32 size;
 	int num_fences, ret, i;
@@ -444,15 +443,9 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 		return -EINVAL;
 
 	size = num_fences * sizeof(*fence_info);
-
-	if (likely(size <= sizeof(fence_info_onstack))) {
-		memset(fence_info_onstack, 0, sizeof(fence_info_onstack));
-		fence_info = fence_info_onstack;
-	} else {
-		fence_info = kzalloc(size, GFP_KERNEL);
-		if (!fence_info)
-			return -ENOMEM;
-	}
+	fence_info = kzalloc(size, GFP_KERNEL);
+	if (!fence_info)
+		return -ENOMEM;
 
 	for (i = 0; i < num_fences; i++) {
 		int status = sync_fill_fence_info(fences[i], &fence_info[i]);
@@ -475,8 +468,7 @@ no_fences:
 		ret = 0;
 
 out:
-	if (unlikely(fence_info != fence_info_onstack))
-		kfree(fence_info);
+	kfree(fence_info);
 
 	return ret;
 }
